d14dc7da55c486bec3e1e955d9984a050b4de14b
Akhtar Zaman <az@zamy.io>
2021-10-25 19:49:06 -0400

    Update is_unique.py

--- CTCI_questions/is_unique.py
+++ CTCI_questions/is_unique.py
@@ -1,3 +1,7 @@
+" Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?"
+
+
+
 def is_unique(string):
     unique_chars = set()
     for c in string:
