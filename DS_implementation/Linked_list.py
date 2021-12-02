a1746b0b0edf273f9fc8a430c36c04bc74b854d6
Aktar Zaman <zamy@Aktars-iMac.fios-router.home>
2021-12-01 22:58:53 -0500

    working on implementing linkedlist

---
+++ DS_implementation/Linked_list.py
@@ -0,0 +1,34 @@
+class Node:
+    def __init__(self, init_data):
+        self.data = init_data
+        self.next = None
+
+    def get_data(self):
+        return self.data
+
+    def get_next(self):
+        return self.next
+
+    def set_data(self, new_data):
+        self.data = new_data
+
+    def set_next(self, new_next):
+        self.next = new_next
+
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+
+    def is_empty(self):
+        return self.head == None
+
+    def add(self, item):
+        temp = Node(item)
+        temp.set_next(self.head)
+        self.head = temp
+
+    def
+
+
